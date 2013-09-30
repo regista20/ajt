8145308ccc2bf245fa715f9ccdb568de187fab5a
yuki <regista20ys@gmail.com>
2013-09-30 16:38:18 +0900

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ajt::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/contact"
+  get "static_pages/faq"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

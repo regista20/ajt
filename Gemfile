cf9b8024f458cd748bb084bc346187b2aa268b3e
yuki <regista20ys@gmail.com>
2013-10-01 13:33:41 +0900

    Finish static pages

--- Gemfile
+++ Gemfile
@@ -5,11 +5,11 @@ gem 'rails', '4.0.0'
 
 group :development, :test do
   gem 'sqlite3', '1.3.7'
-  gem 'rspec-rails', '2.13.1'
+  gem 'rspec-rails', '2.14.0'
 end
 
 group :test do
-  gem 'selenium-webdriver', '2.0.0'
+  gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
 end
 
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'AJT'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('AJT')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("AJT | Home")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do 
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("AJT | About Us")
+    end
+  end
+
+  describe "FAQ page" do
+
+    it "should have the content 'FAQ'" do
+      visit '/static_pages/faq'
+      expect(page).to have_content('FAQ')
+    end
+
+    it "should have the title 'FAQ'" do
+      visit '/static_pages/faq'
+      expect(page).to have_title("AJT | FAQ")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("AJT | Contact")
+    end
+  end
+end
